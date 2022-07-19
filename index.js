6b366cf2e53df987e88bd13bf0bcfe197b7f3188
zack-oss <errortor40@gmail.com>
2022-07-19 11:59:26 +0300

    index.js modified

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 //This is for educational purpose.
 // !!! Its not recommended to store secrets in the source code.
 // !!! You should make use of environment variables.
+// Read all your secrets from environment variables.
 
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
-const CLIENTID =
-  "164694962957-dhgarrgeedns0oai6bvfhlsbofuhr8iv.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URL = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN =
-  "1//04XpIfsnrvP_wCgYIARAAGAQSNwF-L9Ir-hWJVebxhdNp5HrUoppqYUGo_gXU109adNPeOF2L5x6JMlsimmgnxNhpTRCrH8ghH0I";
+const CLIENTID =  "your_client_id";
+const CLIENT_SECRET = "your_client_secret";
+const REDIRECT_URL = "https://redirect_url";
+const REFRESH_TOKEN =  "your_refresh_token";
 
 //creating oAuth2client to be used on our behalf
 const oauth2Client = new google.auth.OAuth2(CLIENTID, CLIENT_SECRET, REDIRECT_URL);
@@ -24,7 +23,7 @@ async function sender() {
     service: "gmail",
     auth: {
       type: "oauth2",
-      user: "errortor40@gmail.com",
+      user: "example@domain.org",
       clientId: CLIENTID,
       clientSecret: CLIENT_SECRET,
       refreshToken: REFRESH_TOKEN,
@@ -33,8 +32,8 @@ async function sender() {
   });
 
   let options = {
-    from: "errortor <errortor40@gmail.com>",
-    to: "katongolelsaac78@gmail.com",
+    from: "username <example@domain.org>",
+    to: "receiver@domain.co",
     subject: "Nodemailer and Google OAuth2",
     text: "It finally works with Nodemailer and GoogleAPI",
     html: "<h1>Nodemailer and GoogleAPI OAuth2 </h1>",
